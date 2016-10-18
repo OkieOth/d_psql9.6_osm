fbf7fb249c13bb6d8e23d064e7e56123e6ced18f
okieoth <eiko.thomas@gmx.net>
2016-10-18 11:25:58 +0200

    extend prepareNewCont.sh with dummy script for db init

--- bin/prepareNewCont.sh
+++ bin/prepareNewCont.sh
@@ -29,11 +29,32 @@ mkdir -p  $destDir/data
 
 mkdir -p  $destDir/import
 cat << README > $destDir/import/README.md
+The 00_prepare_db.sql script is needed to activate postgis for the osm
+database.
+
+1. edit 00_prepare_db.sql.todo and replace the name of the osm_database
+2. mv 00_prepare_db.sql.todo 00_prepare_db.sql
+
 Shell scripts or SQL files in this directory are used to initialize database
 
 Allowed file types:
 *.sh, *.sql, *.sql.gz, *.sql.bz2
 README
+
+cat << README_X > $destDir/import/00_prepare_db.sql.todo
+\connect osm_berlin
+
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SET check_function_bodies = false;
+SET client_min_messages = warning;
+
+CREATE EXTENSION IF NOT EXISTS plpgsql WITH SCHEMA pg_catalog;
+CREATE EXTENSION IF NOT EXISTS postgis WITH SCHEMA public;
+README_X
 
 
 mkdir -p  $destDir/tmp
